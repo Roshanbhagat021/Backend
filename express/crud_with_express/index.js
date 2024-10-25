ce902b8505c409f3a975a6fe99c246b654389ace
Roshan kumar bhagat <roshanking021@gmail.com>
2024-10-25 22:56:45 +0530

    new commit

--- express/crud_with_express/index.js
+++ express/crud_with_express/index.js
@@ -76,11 +76,7 @@ app.patch("/evenUpdate", async (req, res) => {
     }
 })
 
-// route for deleting the files
-// Different commit
-// new commit 
-// Brand new commit for nothing
-// ok new commit
+// route for deleting the files 
 app.delete("/deleteCompleted",async(req,res)=>{
     fs.readFile("db.json","utf-8",(err, data)=>{
         if (err){
